e1f05c203cb55c92cb6cb346da6e06324edb59d2
MatheusZamo <19matheushenrique03@gmail.com>
2024-09-08 11:28:06 -0300

    code base

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,59 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+const days = [
+  "Domingo",
+  "Segunda-Feira",
+  "Terça-Feira",
+  "Quarta-Feira",
+  "Quinta-feira",
+  "Sexta-feira",
+]
+
+const months = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abri",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
+]
+
+const App = () => {
+  const present = new Date()
+  const dayOfWeek = days[present.getDay()]
+  const dayOfMonth = present.getDate()
+  const month = months[present.getMonth() - 1]
+  const year = present.getFullYear()
+
+  return (
+    <div className="container">
+      <div className="count">
+        <button onClick={() => console.log("Clicou no menos do Intervalo")}>
+          -
+        </button>
+        <h2>Intervalo: 1</h2>
+        <button onClick={() => console.log("Clicou no mais do Intervalo")}>
+          +
+        </button>
+      </div>
+      <div className="count">
+        <button onClick={() => console.log("Clicou no menos da Contagem")}>
+          -
+        </button>
+        <h2>Contagem: 0</h2>
+        <button onClick={() => console.log("Clicou no mais da Contagem")}>
+          +
+        </button>
+      </div>
+      <h2>
+        Hoje é {dayOfWeek}, {dayOfMonth} de {month}. de {year}
+      </h2>
+    </div>
+  )
+}
 
 export { App }
